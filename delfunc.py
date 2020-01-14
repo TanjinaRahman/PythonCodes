77b754f699f821b2a9c63bd92d01da595b7d9ac0
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-14 21:22:47 +0600

    Erase whole array

---
+++ delfunc.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 19:51:31 2020
+
+@author: Aspire
+"""
+
+#a = [1,2]
+#b = a
+#print b
+#del a[:]
+#print b
+#print a
+
+a = [1,2]
+b = a
+a = []
+print b
+print a
