e31980f167a542431a0a8658447cf3e302b593bc
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-19 19:31:44 +0600

    random integer input

---
+++ randomIntInaRange.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 09:13:49 2020
+
+@author: Aspire
+"""
+
+
+import random
+
+x=[]
+y=[]
+for z in range(0,20):
+    x.append(random.randrange(0, 15))
+for z in range(0,20):
+    y.append(random.randrange(0, 15))
+
+print (x)
+print (y)
